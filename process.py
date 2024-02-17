c00af67014ab01dbc9407efdfce90cf00e2ec73e
meiys <58581439+meiys@users.noreply.github.com>
2024-02-17 09:42:52 +0800

    Update process.py

--- process.py
+++ process.py
@@ -75,7 +75,7 @@ MT-Network-Type: WIFI
 MT-Token: 1
 MT-Team-ID: 
 MT-Info: 028e7f96f6369cafe1d105579c5b9377
-MT-Device-ID: clips_fht5Sy1LfUl8S39MekkoSnkfJhMqTH9KfkovSi4=
+MT-Device-ID: fd681f73-fa03-4f33-8612-238b19ce1327
 MT-Bundle-ID: com.moutai.mall
 Accept-Language: en-CN;q=1, zh-Hans-CN;q=0.9
 MT-Request-ID: 167560018873318465
@@ -88,7 +88,7 @@ Connection: keep-alive
 Content-Type: application/json
 userId: 2
 '''
-#MT-Device-ID:fd681f73-fa03-4f33-8612-238b19ce1327
+#MT-Device-ID:clips_fht5Sy1LfUl8S39MekkoSnkfJhMqTH9KfkovSi4=
 
 # 初始化请求头
 def init_headers(user_id: str = '1', token: str = '2', lat: str = '29.83826', lng: str = '119.74375'):
